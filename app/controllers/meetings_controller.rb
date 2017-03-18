b504ecf0b67a55c4007918806fbbf0ee61f66d83
Jonathan Hurter <john@scalingo.com>
2017-03-18 23:29:53 +0100

    Add correct verification code

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,6 @@
 class MeetingsController < ApplicationController
   def create
-    verifcation_codes = [123,111,222]
+    verifcation_codes = ["123456123456"]
 
     render json: {"error": "Not signed in"}, status: 418 and return if !user_signed_in?
     render json: {"error": "Invalid verification"}, status: 418 and return unless verifcation_codes.include? params[:verification]
