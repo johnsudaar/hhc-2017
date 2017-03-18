3acec66650735b3a1b0bbdf426b519118c26f381
Anthony Martin <martin.anthonyq@gmail.com>
2017-03-19 00:07:20 +0100

    Update entity_serializer.rb
    Added latitude/longitude to entity serializer

--- app/serializers/entity_serializer.rb
+++ app/serializers/entity_serializer.rb
@@ -1,3 +1,3 @@
 class EntitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :country, :city, :street, :post_code
+  attributes :id, :name, :country, :city, :street, :post_code, :latitude, :longitude
 end
